ad561dd78c74622c63d0d8d65d1017566b7e8cee
Yamin Hossain <streatdot616@gmail.com>
2024-03-06 18:12:52 +0600

    lsdjf

--- routes/partials/auth.routes.js
+++ routes/partials/auth.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const isAuthMiddleware = require("../../app/middlewares/isAuth.middleware");
 const { server_locals } = require("../..");
-// const userModel = require("../../app/models/user.model");
-// const userModel = require("../../app/api/user/model/model");
+
 const router = express.Router();
 const bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken');
@@ -17,50 +16,47 @@ const userModel = db.users
 router
 	.post("/api/login-submit", async function (req, res) {
 		console.log("request form auth routes",req.body);
-		// return;
 		const { email, password } = req.body;
 		console.log('pre pass', password);
-		let user = await userModel.where({ email: email }).findOne();
-		console.log('last pass', user);
-		console.log(user);
-		return ;
+		let user = await userModel.findOne({ where: { email: email } });
+		// console.log('last pass', user);
+		console.log('found user',user);
+		// return ;
 		if (user) {
-			console.log(user);
+			// console.log(user);
 			// console.log(user?.role[0]?.title);
 			// let title = user?.role[0]?.title;
-			let title = user?.username;
-			let role = user?.role;
-			console.log('title', title);
+			let title = user?.dataValues?.user_name;
+			let role = user?.dataValues?.role;
+			console.log('title', title, role);
 			let passMatch = await bcrypt.compare(password, user?.password);
 			console.log(passMatch);
 			// return;
 			if (passMatch) {
 				// console.log(passMatch);
 				let data = {
-					username: user.username,
-					email: user.email,
-					_id: user._id,
-					photo_url: '',
-					device_id: '',
-					genrate_time: '',
+					username: user?.dataValues?.user_name,
+					role: user?.dataValues?.role,
+					id: user?.dataValues?.id,
+					email: user?.dataValues?.email,
 				};
-				// console.log('data', req?.session?.user);
-				req.session.isAuth = true;
-				req.session.user = data;
-				var token = await jwt.sign(data, '91eb159c-a766-48c3-b143-849170dbceb8');
-				res.cookie('token', token)
-				// console.log('prev_auth_url',req.session.prev_auth_url);
-				let prevUrl = req.session.prev_auth_url;
-				console.log('prevurl',prevUrl);
-				console.log('token',token);
-				// return;
-				if (prevUrl) {
-					delete req.session.prev_auth_url;
-					if (prevUrl != "/favicon.ico") {
-						return res.redirect(prevUrl);
-					}
-				}
-				return res.status(201).json({ code: 'password match', message: 'your password match', title: title, role: role });
+				// // console.log('data', req?.session?.user);
+				// req.session.isAuth = true;
+				// req.session.user = data;
+				// var token = await jwt.sign(data, '91eb159c-a766-48c3-b143-849170dbceb8');
+				// res.cookie('token', token)
+				// // console.log('prev_auth_url',req.session.prev_auth_url);
+				// let prevUrl = req.session.prev_auth_url;
+				// console.log('prevurl',prevUrl);
+				// console.log('token',token);
+				// // return;
+				// if (prevUrl) {
+				// 	delete req.session.prev_auth_url;
+				// 	if (prevUrl != "/favicon.ico") {
+				// 		return res.redirect(prevUrl);
+				// 	}
+				// }
+				return res.status(201).json({ code: 'password match', message: 'your password match', data});
 			}
 			return res.status(401).json({ code: 'password does not match', message: 'your crediential does not match' });
 		}
