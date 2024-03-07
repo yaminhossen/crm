6727df97d29fa5787ab3bf845457379e6ede79f8
Yamin Hossain <streatdot616@gmail.com>
2024-03-07 14:33:56 +0600

    last updared

--- routes/partials/auth.routes.js
+++ routes/partials/auth.routes.js
@@ -14,48 +14,45 @@ const userModel = db.users
 
 
 router
+// login method
 	.post("/api/login-submit", async function (req, res) {
-		console.log("request form auth routes",req.body);
+		// console.log("request form auth routes",req.body);
 		const { email, password } = req.body;
-		console.log('pre pass', password);
+		// console.log('pre pass', password);
 		let user = await userModel.findOne({ where: { email: email } });
 		// console.log('last pass', user);
-		console.log('found user',user);
+		// console.log('found user',user);
 		// return ;
 		if (user) {
-			// console.log(user);
-			// console.log(user?.role[0]?.title);
-			// let title = user?.role[0]?.title;
-			let title = user?.dataValues?.user_name;
-			let role = user?.dataValues?.role;
-			console.log('title', title, role);
 			let passMatch = await bcrypt.compare(password, user?.password);
-			console.log(passMatch);
 			// return;
 			if (passMatch) {
-				// console.log(passMatch);
 				let data = {
 					username: user?.dataValues?.user_name,
 					role: user?.dataValues?.role,
 					id: user?.dataValues?.id,
 					email: user?.dataValues?.email,
 				};
-				// // console.log('data', req?.session?.user);
-				// req.session.isAuth = true;
-				// req.session.user = data;
-				// var token = await jwt.sign(data, '91eb159c-a766-48c3-b143-849170dbceb8');
-				// res.cookie('token', token)
-				// // console.log('prev_auth_url',req.session.prev_auth_url);
-				// let prevUrl = req.session.prev_auth_url;
+				req.session.isAuth = true;
+				req.session.user = data;
+				console.log('data', req?.cookies);
+				var token = await jwt.sign(data, 'yamin1234');
+				// console.log('token form ', token);
+				res.cookie('token', token)
+				console.log('data', req?.session);
+				// console.log('res', res);
+				// console.log('prev_auth_url',res);
+				let prevUrl = req.session.prev_auth_url;
 				// console.log('prevurl',prevUrl);
-				// console.log('token',token);
-				// // return;
-				// if (prevUrl) {
-				// 	delete req.session.prev_auth_url;
-				// 	if (prevUrl != "/favicon.ico") {
-				// 		return res.redirect(prevUrl);
-				// 	}
-				// }
+				console.log('tokenee',token);
+				// console.log('session',req.session.user);
+				// return;
+				if (prevUrl) {
+					delete req.session.prev_auth_url;
+					if (prevUrl != "/favicon.ico") {
+						return res.redirect(prevUrl);
+					}
+				}
 				return res.status(201).json({ code: 'password match', message: 'your password match', data});
 			}
 			return res.status(401).json({ code: 'password does not match', message: 'your crediential does not match' });
@@ -64,7 +61,9 @@ router
 		// return res.redirect("/login");
 		return res.status(401).json({ code: 'user not found', message: 'your crediential does not match' });;
 	})
-	.post("/signup-submit", async function (req, res) {
+
+	// signup method
+	/* .post("/signup-submit", async function (req, res) {
 		// console.log(req.body);
 		const { username, email, password, password_confirmation } = req.body;
 		let error = {};
@@ -107,8 +106,13 @@ router
 			return res.redirect(prevUrl);
 		}
 		res.redirect("/dashboard");
-	})
+	}) */
+
+
+	// isauth method
 	// .use(isAuthMiddleware())
+
+	// logout method
 	.post("/logout", function (req, res) {
 		console.log("logoutlogo9ut");
 		req.session.isAuth = false;
