e88f84b38d91295538faaf7587e273a95465da74
Yamin Hossain <streatdot616@gmail.com>
2024-04-21 19:32:29 +0600

    ldsjfl

--- app/api/user/user_work_users/controller/controller.js
+++ app/api/user/user_work_users/controller/controller.js
@@ -94,6 +94,7 @@ const update = async (req, res) => {
     
     let id = req.body.id
     // let id = req.params.id
+    console.log('user work user ', req.body);
     const item = await DataTable.update(req.body, { where: { id: id }})
     res.status(200).send(item)
 }
